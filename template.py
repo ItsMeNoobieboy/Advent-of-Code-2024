8cbbe19c7be06dcbdc84a8066357f7184bb5ac12
ItsMeNoobieboy <54125080+ItsMeNoobieboy@users.noreply.github.com>
2024-12-01 22:49:14 -0500

    Add template

---
+++ template.py
@@ -0,0 +1,22 @@
+import os, sys, re, math, itertools, functools, collections, operator
+
+input_file = os.path.join(os.path.dirname(__file__), "input.txt")
+with open(input_file, "r") as f:
+    lines = f.readlines()
+
+
+def part_1() -> str | int:
+    total = 0
+
+    return total
+
+
+def part_2() -> str | int:
+    total = 0
+
+    return total
+
+
+if __name__ == "__main__":
+    print("Part 1:", part_1())
+    print("Part 2:", part_2())
